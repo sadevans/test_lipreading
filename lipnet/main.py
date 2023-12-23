99250e7c3017f61efb81d78a499a1399b7e9b338
sasha <a.s.gorokhova@yandex.ru>
2023-12-23 13:54:39 +0300

    добавила main для теста

---
+++ lipnet/main.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+import math
+import os
+import sys
+from dataset import MyDataset
+import numpy as np
+import time
+from model import LipNet
+import torch.optim as optim
+import re
+import json
+
+
+if __name__ == '__main__':
+    opt = __import__('options')
+    os.environ['CUDA_VISIBLE_DEVICES'] = opt.gpu
+
+    model = LipNet()
+    model = model.cuda()
+
+    net = nn.DataParallel(model).cuda()
+    if(hasattr(opt, 'weights')):
+        pretrained_dict = torch.load(opt.weights)
+        model_dict = model.state_dict()
+        pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict.keys() and v.size() == model_dict[k].size()}
+        missed_params = [k for k, v in model_dict.items() if not k in pretrained_dict.keys()]
+        print('loaded params/tot params:{}/{}'.format(len(pretrained_dict),len(model_dict)))
+        print('miss matched params:{}'.format(missed_params))
+        model_dict.update(pretrained_dict)
+        model.load_state_dict(model_dict)
+
+    else:
+        print('Please set pretrained weights')
