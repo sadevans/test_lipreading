3fb705d0d8438fcfa3b17f206967bf1ed706aae8
sasha <a.s.gorokhova@yandex.ru>
2023-12-24 00:20:02 +0300

    добавила доп зависимости для auto-vsr

---
+++ install_additional.py
@@ -0,0 +1,6 @@
+import subprocess
+
+subprocess.run(['git', 'clone', 'https://github.com/pytorch/fairseq'])
+subprocess.run(['cd', 'fairseq'])
+subprocess.run(['pip', 'install', '--editable', './'])
+subprocess.run(['cd', '..'])
