8501d6f8bda5a4f8e8aa392717737fba0b56e3c7
sasha <a.s.gorokhova@yandex.ru>
2023-12-24 19:59:37 +0300

    подредачила запуск

--- demo.py
+++ demo.py
@@ -5,7 +5,6 @@ if len(sys.argv) > 1:
     model = sys.argv[1]
 
     if model  == 'lipnet':
-        print('Model lipnet')
         if len(sys.argv) == 4:
             print(sys.argv[3])
             subprocess.run(['python', 'lipnet/main.py', sys.argv[2], sys.argv[3]])
@@ -14,16 +13,14 @@ if len(sys.argv) > 1:
             exit
     elif model == 'auto vsr':
         print('Model auto vsr')
+        # if len(sys.argv) != 4:
+        #     print('Необходимо ввести путь к видео и к модели')
         if len(sys.argv) == 4:
-            subprocess.run(['python', 'auto_vsr/main.py', 'data.modality=video', 'file_path=',sys.argv[2], \
-                            'pretrained_model_path=',sys.argv[3]])
+            subprocess.run(['python', 'auto_vsr/main.py', 'data.modality=video', 'file_path='+sys.argv[2], \
+                            'pretrained_model_path='+sys.argv[3]])
         elif len(sys.argv) == 5:
-            subprocess.run(['python', 'auto_vsr/main.py', 'data.modality=video', 'file_path=',sys.argv[2], \
-                            'anno_path=',sys.argv[3], 'pretrained_model_path=',sys.argv[4]])
-        else:
-            print('Необходимо ввести путь к видео и к модели')
-            exit
+            subprocess.run(['python', 'auto_vsr/main.py', 'data.modality=video', 'file_path='+sys.argv[2], \
+                            'anno_path='+sys.argv[3], 'pretrained_model_path='+sys.argv[4]])
 
 else:
     print('Необходимо передать имя тестируемой модели')
-    exit
