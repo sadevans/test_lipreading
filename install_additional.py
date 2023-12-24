36c0175775d90d325e5399a7e48865418c1c1a67
sasha <a.s.gorokhova@yandex.ru>
2023-12-24 12:30:46 +0300

    редактирование install_additionals

--- install_additional.py
+++ install_additional.py
@@ -1,10 +1,19 @@
 import subprocess
 
-def install_additional():
-    subprocess.run(['git', 'clone', 'https://github.com/pytorch/fairseq'])
-    subprocess.run(['cd', 'fairseq'])
-    subprocess.run(['pip', 'install', '--editable', './'])
-    subprocess.run(['cd', '..'])
+# def install_additional():
+#     subprocess.run(['git', 'clone', 'https://github.com/pytorch/fairseq'])
+#     subprocess.run(['cd', 'fairseq'])
+#     subprocess.run(['pip', 'install', '--editable', './'])
+#     subprocess.run(['cd', '..'])
+
+subprocess.run(['ls'], cwd='auto_vsr', shell=True)
+subprocess.run(['git', 'clone', 'https://github.com/pytorch/fairseq'])
+# subprocess.run(['%cd', 'fairseq'])
+subprocess.run(['ls'], cwd='fairseq', shell=True)
+
+subprocess.run(['pip', 'install', '--editable', './'])
+# subprocess.run(['%cd', '..'])
+subprocess.run(['ls'], cwd='auto_vsr', shell=True)
 
 # subprocess.run(['git', 'clone', 'https://github.com/pytorch/fairseq'])
 # subprocess.run(['cd', 'fairseq'])
