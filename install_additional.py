fa9b2045290f7878d0d41dad1e33569bff69d1e6
sasha <a.s.gorokhova@yandex.ru>
2023-12-24 19:39:53 +0300

    подредачила доп зависимости

--- install_additional.py
+++ install_additional.py
@@ -2,30 +2,25 @@ import subprocess
 import os
 
 subprocess.run(['pip', 'install', 'hydra-core', '--upgrade'])
-# subprocess.run('cd ./auto_vsr && ls', shell=True)
-os.chdir('auto_vsr')
-subprocess.run(['git', 'clone', 'https://github.com/pytorch/fairseq'])
-# subprocess.run('cd ./fairseq && ls', shell=True)
-os.chdir('fairseq')
-subprocess.run(['pip', 'install', '--editable', './'])
-# subprocess.run('cd .. && ls', shell=True)
-os.chdir('..')
-subprocess.run(['git', 'clone', 'https://github.com/hhj1897/face_alignment.git'])
-# subprocess.run('cd ./face_alignment && ls', shell=True)
-os.chdir('face_alignment')
-subprocess.run(['pip', 'install', '-e', '.'])
-# subprocess.run('cd .. && ls', shell=True)
-os.chdir('..')
 
+if not os.path.exists('./auto_vsr/fairseq'):
+    os.chdir('auto_vsr')
+    subprocess.run(['git', 'clone', 'https://github.com/pytorch/fairseq'])
+    os.chdir('fairseq')
+    subprocess.run(['pip', 'install', '--editable', './'])
+    os.chdir('..')
+if not os.path.exists('./auto_vsr/face_alignment'):
 
-subprocess.run(['git', 'clone', 'https://github.com/hhj1897/face_detection.git'])
-# subprocess.run('cd ./face_detection && ls', shell=True)
-os.chdir('face_detection')
+    subprocess.run(['git', 'clone', 'https://github.com/hhj1897/face_alignment.git'])
+    os.chdir('face_alignment')
+    subprocess.run(['pip', 'install', '-e', '.'])
+    os.chdir('..')
 
-subprocess.run(['git', 'lfs', 'pull'])
-subprocess.run(['pip', 'install', '-e', '.'])
-os.chdir('..')
-os.chdir('..')
+if not os.path.exists('./auto_vsr/face_detection'):
+    subprocess.run(['git', 'clone', 'https://github.com/hhj1897/face_detection.git'])
+    os.chdir('face_detection')
 
-# subprocess.run('cd .. && ls', shell=True)
-# subprocess.run('cd .. && ls', shell=True)
+    subprocess.run(['git', 'lfs', 'pull'])
+    subprocess.run(['pip', 'install', '-e', '.'])
+    os.chdir('..')
+    os.chdir('..')
