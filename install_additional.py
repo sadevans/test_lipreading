bcdb07ac9c926c8946b300cef64aa193be344d85
sasha <a.s.gorokhova@yandex.ru>
2023-12-24 12:46:30 +0300

    редактирование зависимостей

--- install_additional.py
+++ install_additional.py
@@ -1,21 +1,15 @@
 import subprocess
-
-# def install_additional():
-#     subprocess.run(['git', 'clone', 'https://github.com/pytorch/fairseq'])
-#     subprocess.run(['cd', 'fairseq'])
-#     subprocess.run(['pip', 'install', '--editable', './'])
-#     subprocess.run(['cd', '..'])
-
-subprocess.run(['ls'], cwd='auto_vsr', shell=True)
+subprocess.run(['ls'], cwd='lipreading/auto_vsr', shell=True)
 subprocess.run(['git', 'clone', 'https://github.com/pytorch/fairseq'])
-# subprocess.run(['%cd', 'fairseq'])
-subprocess.run(['ls'], cwd='fairseq', shell=True)
-
+subprocess.run(['ls'], cwd='auto_vsr/fairseq', shell=True)
 subprocess.run(['pip', 'install', '--editable', './'])
-# subprocess.run(['%cd', '..'])
-subprocess.run(['ls'], cwd='auto_vsr', shell=True)
+subprocess.run(['ls'], cwd='lipreading/auto_vsr', shell=True)
 
-# subprocess.run(['git', 'clone', 'https://github.com/pytorch/fairseq'])
-# subprocess.run(['cd', 'fairseq'])
-# subprocess.run(['pip', 'install', '--editable', './'])
-# subprocess.run(['cd', '..'])
+
+# subprocess.run(['ls'], cwd='lipreading/auto_vsr', shell=True)
+subprocess.run(['git', 'clone', 'https://github.com/hhj1897/face_detection.git'])
+subprocess.run(['ls'], cwd='auto_vsr/face_detection', shell=True)
+subprocess.run(['git', 'lfs', 'pull'])
+
+subprocess.run(['pip', 'install', '-e'])
+subprocess.run(['ls'], cwd='lipreading/auto_vsr', shell=True)
